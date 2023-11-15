d142639487ff5f5e9a5fa01a109901972d71ff3b
_boy4uk <116024276+oleksiyboichuk@users.noreply.github.com>
2023-11-15 20:10:37 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'redyan-100': 'hsl(90, 18%, 64%)',
+        'redyan-200': 'hsl(148, 30%, 46%)',
+        'redyan-300': 'hsl(157, 16%, 28%)',
+        'redyan-400': 'hsl(320, 15%, 19%)',
+      }
+    },
   },
   plugins: [],
 }
